c6bcdfd36ca864ff0c5786c06458e936369eac8f
Timothy Luebke <luebketj@gmail.com>
2021-10-27 14:16:15 -0600

    finished step 3 adding a delete button

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 console.log("Hello World");
+
+function deleteMovie(event){
+    event.target.parentNode.remove();
+    
+}
+
+
 function addMovie(evt){
-    console.log("atempting to add a movie");
+    //stop the submit from sending us somehwere else
     evt.preventDefault();
+    //get what the user has entered and create a new list element with that value
     let inputField = document.querySelector("input");
     let movie = document.createElement("li");
     let movieTitle = document.createElement("span");
     console.log(inputField.value);
     movieTitle.textContent = (inputField.value);
+    //set the value of inputfield to nothing as to clear it for the next input
     inputField.value = "";
-    movie.appendChild(movieTitle);
 
+    //make sure we add these elements to the document
+    movie.appendChild(movieTitle);
     let ulEl = document.querySelector("ul");
     ulEl.appendChild(movie);
+
+    //create a deletion button
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.addEventListener('click',deleteMovie);
+    movie.appendChild(deleteBtn);
 }
 
 let form = document.querySelector("form");
