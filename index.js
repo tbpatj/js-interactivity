ba7daa41914c8b441a8de611f9e627653726eb17
Timothy Luebke <luebketj@gmail.com>
2021-10-27 14:19:06 -0600

    added the cross off functionality

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function deleteMovie(event){
     event.target.parentNode.remove();
     
 }
-
+function crossOffMovie(event){
+    event.target.classList.toggle("checked");
+}
 
 function addMovie(evt){
     //stop the submit from sending us somehwere else
@@ -17,6 +19,9 @@ function addMovie(evt){
     movieTitle.textContent = (inputField.value);
     //set the value of inputfield to nothing as to clear it for the next input
     inputField.value = "";
+
+    //make sure the span has an event listener so we can cross of if clicked
+    movieTitle.addEventListener('click',crossOffMovie);
 
     //make sure we add these elements to the document
     movie.appendChild(movieTitle);
