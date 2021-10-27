c8771a2e4fff3b8770f78f1138c5ad20d9227939
Timothy Luebke <luebketj@gmail.com>
2021-10-27 14:09:12 -0600

    finished step 2

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("Hello World");
+function addMovie(evt){
+    console.log("atempting to add a movie");
+    evt.preventDefault();
+    let inputField = document.querySelector("input");
+    let movie = document.createElement("li");
+    let movieTitle = document.createElement("span");
+    console.log(inputField.value);
+    movieTitle.textContent = (inputField.value);
+    inputField.value = "";
+    movie.appendChild(movieTitle);
+
+    let ulEl = document.querySelector("ul");
+    ulEl.appendChild(movie);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit",addMovie);
